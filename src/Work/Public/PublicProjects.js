86ed3b372ce6946ea7d07072df2821b093288e6a
BendianaJ <benjaminrandfilm@gmail.com>
2020-06-19 11:53:25 -0600

    js started for window expand

---
+++ src/Work/Public/PublicProjects.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../App.css';
+
+
+
+function projects () 
+{
+  const projects =
+  {
+    projectWindow: 
+    [
+      {Project1: <div className="w-full project-box project-1">project1</div>},
+      {Project2: <div className="w-full project-box project-1">project2</div>},
+      {Project3: <div className="w-full project-box project-1">project3</div>}
+    ] 
+  }
+
+  let currentProject;
+
+}
