6f0160dabf5d6b8be04b5a5bd276174dced39228
BendianaJ <benjaminrandfilm@gmail.com>
2020-06-24 16:05:35 -0600

    changed to react class added project controls

--- src/Work/Public/PublicProject.js
+++ src/Work/Public/PublicProject.js
@@ -2,46 +2,87 @@ import React from 'react';
 // import CloseBox from '../CloseBox';
 import '../../App.css';
 
-function PublicProject (props)
+
+class PublicProject extends React.Component
 {
-  const handleClick = () => {
-    props.handleClick(props.index)
-    console.log(props.settings)
+  state={activeSlide:0}
+
+  handleClick = () => {
+    this.props.handleClick(this.props.index)
+    // console.log(props.settings)
   }
 
-  const closeBox = () => {
-    props.closeBox(props.index)
-    console.log(props.settings)
+  closeBox = () => {
+    this.props.closeBox(this.props.index)
+    this.setState({activeSlide:0})
+    console.log(this.props.settings)
   }
 
-  return(
-    <div
-      id={props.name}
-      className=
-      {
-        (props.isActive && props.isTransition) ? 'a-transition' : 
-        props.isActive ? 'active' : 
-        (!props.isActive && props.isTransition) ? 'b-transition' : 
-        props.anyActive ? 'inactive' : 'project-box'
-      }
-    >
-      <div 
-        onClick={handleClick}
-        className={props.isActive ? 'off-project' : props.anyActive ? 'off-project' : 'select-project'}
-      >
-      {props.name}
-      </div>
-      <div onClick={closeBox}
+  nextSlide = () => {
+    let slideIndex = this.state.activeSlide;
+    (slideIndex < 2) ? slideIndex++ : (slideIndex = 0)
+    this.setState({activeSlide: slideIndex})
+  }
+
+  prevSlide = () => {
+    let slideIndex = this.state.activeSlide;
+    (slideIndex > 0) ? slideIndex-- : (slideIndex = 2)
+    this.setState({activeSlide: slideIndex})
+  }
+  
+  render(){     
+    let projectImages = ['photo1', 'photo2', 'photo3']   
+    return(
+      <div
+        id={this.props.name}
         className=
         {
-          (props.isActive && props.isTransition) ? '' :
-          props.isActive ? 'close-button' : ''
+          (this.props.isActive && this.props.isTransition) ? `a-transition ${projectImages[this.state.activeSlide]}` : 
+          this.props.isActive ? `active ${projectImages[this.state.activeSlide]}` : 
+          (!this.props.isActive && this.props.isTransition) ? `b-transition ${projectImages[this.state.activeSlide]}` : 
+          this.props.anyActive ? `inactive ${projectImages[this.state.activeSlide]}` : `project-box ${projectImages[this.state.activeSlide]}`
         }
       >
-      </div>
-    </div>
-  )
 
+        <div id="project-activate-window"
+          onClick={this.handleClick}
+          className={'select-project'}
+        >
+
+          {/* {projectImages[this.state.activeSlide]} */}
+
+          <div id="close-box"
+            onClick={this.closeBox}
+            className=
+            {
+              (this.props.isActive && this.props.isTransition) ? '' :
+              this.props.isActive ? 'close-button' : ''
+            }
+          ></div>
+          <div className="flex-row j-center">
+            <div
+            className=
+            {
+              (this.props.isActive && this.props.isTransition) ? '' :
+              this.props.isActive ? 'slideControls' : ''
+            }
+            onClick={this.prevSlide}></div>
+            <div 
+            className=
+            {
+              (this.props.isActive && this.props.isTransition) ? '' :
+              this.props.isActive ? 'slideControls' : ''
+            }
+            onClick={this.nextSlide}></div>
+          </div>
+
+        </div>
+
+
+
+      </div>
+    )
+  }          
 }
 
 export default PublicProject;
