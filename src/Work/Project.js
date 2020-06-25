4f2c3ee55f501e7c7feae1378ae5002e8cd2c7c0
BendianaJ <benjaminrandfilm@gmail.com>
2020-06-25 09:05:35 -0600

    fully functional, projects passed from work pages

---
+++ src/Work/Project.js
@@ -0,0 +1,88 @@
+import React from 'react';
+// import CloseBox from '../CloseBox';
+import '../App.css';
+
+
+class Project extends React.Component
+{
+  state={activeSlide:0}
+
+  handleClick = () => {
+    this.props.handleClick(this.props.index)
+    // console.log(props.settings)
+  }
+
+  closeBox = () => {
+    this.props.closeBox(this.props.index)
+    this.setState({activeSlide:0})
+    console.log(this.props.settings)
+  }
+
+  nextSlide = () => {
+    let slideIndex = this.state.activeSlide;
+    (slideIndex < 2) ? slideIndex++ : (slideIndex = 0)
+    this.setState({activeSlide: slideIndex})
+  }
+
+  prevSlide = () => {
+    let slideIndex = this.state.activeSlide;
+    (slideIndex > 0) ? slideIndex-- : (slideIndex = 2)
+    this.setState({activeSlide: slideIndex})
+  }
+  
+  render(){     
+      
+    return(
+      <div
+        id={this.props.name}
+        className=
+        {
+          (this.props.isActive && this.props.isTransition) ? `a-transition ${this.props.projectPhotos[this.state.activeSlide]}` : 
+          this.props.isActive ? `active ${this.props.projectPhotos[this.state.activeSlide]}` : 
+          (!this.props.isActive && this.props.isTransition) ? `b-transition ${this.props.projectPhotos[this.state.activeSlide]}` : 
+          this.props.anyActive ? `inactive ${this.props.projectPhotos[this.state.activeSlide]}` : `project-box ${this.props.projectPhotos[this.state.activeSlide]}`
+        }
+      >
+
+        <div id="project-activate-window"
+          onClick={this.handleClick}
+          className={'select-project'}
+        >
+
+          {/* {this.props.projectPhotos[this.state.activeSlide]} */}
+
+          <div id="close-box"
+            onClick={this.closeBox}
+            className=
+            {
+              (this.props.isActive && this.props.isTransition) ? '' :
+              this.props.isActive ? 'close-button' : ''
+            }
+          ></div>
+          <div className="flex-row j-center">
+            <div
+            className=
+            {
+              (this.props.isActive && this.props.isTransition) ? '' :
+              this.props.isActive ? 'slideControls' : ''
+            }
+            onClick={this.prevSlide}></div>
+            <div 
+            className=
+            {
+              (this.props.isActive && this.props.isTransition) ? '' :
+              this.props.isActive ? 'slideControls' : ''
+            }
+            onClick={this.nextSlide}></div>
+          </div>
+
+        </div>
+
+
+
+      </div>
+    )
+  }          
+}
+
+export default Project;
