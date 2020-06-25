f28d61b1d803bdb878a1c840a5a579c20b36f3e3
BendianaJ <benjaminrandfilm@gmail.com>
2020-06-25 15:48:10 -0600

    inHome state added  used for dynamic layout

--- src/App.js
+++ src/App.js
@@ -7,24 +7,45 @@ import HomeContent from './Home/HomeContent';
 import Nav from './Nav';
 import './App.css';
 
-function App() {
+class App extends React.Component 
+{
+  state={inContent: false, inHome: true}
 
-  return (
-    <div className="App">
-      <Router>
-      <Nav/>
-      <div className="content">
-        <Switch>
-          <Route path="/Work" component={WorkContent}/>
-          <Route path="/About" component={AboutContent}/>
-          <Route path="/Contact" component={ContactContent}/>
-          <Route path="/" component={HomeContent}/>
+  pageClick = () => {
+    this.setState({inContent:true, inHome:false}) 
+    console.log(this.state.inContent) 
+}
 
-        </Switch>
+  menuClick= () => this.setState({inContent: false, inHome: false})
+
+  homeClick = () => {
+    this.setState({inContent: false, inHome:true})
+  }
+  render()
+  {
+    return (
+      <div className="App">
+        <Router>
+        <Nav pageClick={this.pageClick} homeClick={this.homeClick} inContent={this.state.inContent}/>
+        <div onClick={this.menuClick} className={this.state.inContent ? 'menu-button' : 'noClass'}></div>
+        <div className={this.state.inHome ? 'content' :
+          this.state.inContent && !this.state.inHome ? 'c-selected' : 'content'  
+        }>
+          <div className={this.state.inHome ? 'f-opac' :
+          this.state.inContent && !this.state.inHome ? 'f-opac' : 'h-opac'  
+        }>
+          <Switch>
+            <Route path="/Work/Public" component={WorkContent}/>
+            <Route path="/About" component={AboutContent}/>
+            <Route path="/Contact" component={ContactContent}/>
+            <Route path="/" component={HomeContent}/>
+          </Switch>
+          </div>
+        </div>
+        </Router>
       </div>
-      </Router>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
