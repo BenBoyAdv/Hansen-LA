aaf5b24820fe5c1309f84a1a79870d44b5e4fa6b
BendianaJ <benjaminrandfilm@gmail.com>
2020-06-27 00:51:36 -0600

    nav now single link component

--- src/Nav.js
+++ src/Nav.js
@@ -2,40 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 
-class Nav extends React.Component 
+class NavLink extends React.Component 
 {
-  pageClick = () => this.props.pageClick()
-  homeClick = () => this.props.homeClick()
-  workClick= () => this.props.workClick()
-  navLinks = [
-    {name: 'HOME', divClass: 'link-text', class1: 'i-block nav-link', onClick: this.homeClick, to: '/Home'},
-    {name: 'WORK', divClass: 'link-text', class1: 'i-block nav-link', onClick: this.workClick, to: '/Work/Public'},
-    {name: 'ABOUT', divClass: 'link-text', class1: 'i-block nav-link', onClick: this.pageClick, to: '/About'},
-    {name: 'CONTACT', divClass: 'link-text', class1: 'i-block nav-link', onClick: this.pageClick, to: '/Contact'},
-  ]
+  pageClick = () => this.props.pageClick(this.props.index)
+ 
   render()
   {
     return (
-      // <div className="nav-div">
-          <nav className='nav-bar'>
-            <div className="icon-wrap"><div className="icon i-block"></div></div>
-            <div className="company-wrap i-block"><div className='company-title'>Hansen LA</div></div>
-            <div className="div-wrap"><div className="line-divider i-block"></div></div>
-            
-            {this.navLinks.map((proj) => {
-              return (<Link
-                className={proj.class1} 
-                onClick={proj.onClick} 
-                to={proj.to}
-              >
-              <div className={proj.divClass}>{proj.name}</div>
-              </Link>)
-            })}
-
-          </nav>
-      // </div>
+          
+      <Link
+        className={this.props.class1} 
+        onClick={this.pageClick} 
+        to={this.props.destPath}
+      >
+        <div className={this.props.divClass}>{this.props.name}</div>
+      </Link>
     )
   }
 }
 
-export default Nav;
+export default NavLink;
