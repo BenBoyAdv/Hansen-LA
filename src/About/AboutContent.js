8641f87f56c78dea04424bca2611ab05049f1ae9
BendianaJ <benjaminrandfilm@gmail.com>
2020-06-30 19:21:25 -0600

    added photo and bio divs

--- src/About/AboutContent.js
+++ src/About/AboutContent.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Photo from '../Assets/HLA_ABOUT_TEMP.png'
 
 class AboutContent extends React.Component {
     render() {
-        return <h1>About Stuff</h1>
+        return (
+            <div className="about-container">
+                <div className="about-photo">
+                    <img src={Photo} alt="Greg"/>
+                </div>
+                <div className="about-text">
+                Greg Hansen began his lifelong interest in the outdoor environment while at Huntingdon College,
+where he studied environmental sciences. Even though he majored in English, he continued his
+education at the University of Georgia where he received a degree in landscape architecture.
+After graduation he returned to Alabama and began his landscape architectural career. Having
+worked for several years in offices that specialized in urban design Greg began his own practice
+in 2000. Over the past 20 years his work has focused on quality work both in public and private
+practice.
+                </div>
+            </div>
+        )
     }
 }
 
