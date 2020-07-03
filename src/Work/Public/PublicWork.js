63dd17bd12478b17b95addbda98c72b5925c50bb
BendianaJ <benjaminrandfilm@gmail.com>
2020-07-03 17:04:35 -0600

    linked proj photo style sheet

--- src/Work/Public/PublicWork.js
+++ src/Work/Public/PublicWork.js
@@ -1,15 +1,25 @@
 import React from 'react';
-// import PublicProject from './PublicProject';
 import '../../App.css';
+import '../ProjectPhotos.css';
 import ProjectContainer from '../ProjectContainer';
 
 class PublicWork extends React.Component 
 {
     publicProjects = 
     [
-        {name: "project-1", projectPhotos:['photo1', 'photo2','photo3']},
-        {name: "project-2", projectPhotos:['photo1', 'photo2','photo3']},
-        {name: "project-3", projectPhotos:['photo1', 'photo2','photo3']}
+        {name: "project-1", projectPhotos:[
+            'childrens-park-photo-1', 'childrens-park-photo-2','childrens-park-photo-3',
+            'childrens-park-photo-4', 'childrens-park-photo-5', 'childrens-park-photo-6', 'childrens-park-photo-7'
+        ]},
+        {name: "project-2", projectPhotos:[
+            'childrens-park-photo-4', 'childrens-park-photo-2','childrens-park-photo-5',
+            'childrens-park-photo-1', 'childrens-park-photo-3', 'childrens-park-photo-6', 'childrens-park-photo-7'
+        ]},
+        {name: "project-3", projectPhotos:[
+            'childrens-park-photo-7', 'childrens-park-photo-2','childrens-park-photo-5',
+            'childrens-park-photo-1', 'childrens-park-photo-3', 'childrens-park-photo-6', 'childrens-park-photo-4'
+        ]}
+        
     ]
     render()
     {
